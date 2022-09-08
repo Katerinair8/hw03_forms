cead0e3dce41f622de3ca63466e74935e16642ec
Екатерина Василевич <katerinair8@mail.ru>
2022-09-08 11:38:45 +0200

    Исправления к проекту 4 спринта

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from pytils.translit import slugify
-
 from django.contrib.auth import get_user_model
 
 
@@ -31,11 +29,6 @@ class Group(models.Model):
 
     def __str__(self):
         return self.title
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)[:100]
-        super().save(*args, **kwargs)
 
 
 class Post(models.Model):
