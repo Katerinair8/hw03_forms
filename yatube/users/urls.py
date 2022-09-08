7896a67bd72805c154b21cdf8337ef9956bd48bd
Екатерина Василевич <katerinair8@mail.ru>
2022-09-08 11:21:29 +0200

    Исправления к проекту 4 спринта

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,5 +1,6 @@
-from crypt import methods
-from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.views import LoginView, LogoutView
+
+from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
 
 from django.urls import path
 
@@ -21,12 +22,16 @@ urlpatterns = [
     ),
     path(
         'password_change/',
-        PasswordChangeView.as_view(template_name='users/password_change_form.html'),
+        PasswordChangeView.as_view(
+            template_name='users/password_change_form.html'
+            ),
         name='password_change',
     ),
     path(
         'password_change/done/',
-        PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),
+        PasswordChangeDoneView.as_view(
+            template_name='users/password_change_done.html'
+            ),
         name='password_change_done',
     ),
 ]
